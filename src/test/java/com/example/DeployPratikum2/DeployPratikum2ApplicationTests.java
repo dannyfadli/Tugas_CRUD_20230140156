724b8b5f0cd4b725ee3ae3d54a46f0ed883585ec
danny fadli <danyfdly95@gmail.com>
2026-03-13 14:43:04 +0700

    tambah unit test untuk verifikasi load context aplikasi

---
+++ src/test/java/com/example/DeployPratikum2/DeployPratikum2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DeployPratikum2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployPratikum2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

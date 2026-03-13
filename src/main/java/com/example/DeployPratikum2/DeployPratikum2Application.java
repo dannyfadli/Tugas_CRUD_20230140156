e67c011f488f7d6a861a5d0063aab8170a6018ae
danny fadli <danyfdly95@gmail.com>
2026-03-13 14:32:57 +0700

    buat entry point aplikasi Spring Boot

---
+++ src/main/java/com/example/DeployPratikum2/DeployPratikum2Application.java
@@ -0,0 +1,15 @@
+package com.example.DeployPratikum2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployPratikum2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployPratikum2Application.class, args);
+	}
+
+}
+
+
